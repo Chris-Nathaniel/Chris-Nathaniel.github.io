335bcdaa605a2297847bd683073d13e594a5fde0
Chris <Chris.nathaniel29@gmail.com>
2025-02-07 15:48:21 +0700

    x

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session, redirect, flash, jsonify, current_app, url_for
+from flask import Flask, render_template, request, session, redirect, flash, jsonify, current_app, url_for, send_file
 import shutil
 import os
 import random
@@ -32,6 +32,13 @@ def update_gif():
 def main():
     update_gif()
     return redirect('https://osu.ppy.sh/users/19921620')
+
+@app.route('/dynamic_execute.gif')
+def serve_gif():
+    """Serve execute.gif with a timestamp-based cache buster."""
+    update_gif()  # Ensure GIF updates
+    timestamp = int(time.time())  # Get current timestamp
+    return send_file(EXECUTE_FILE, mimetype='image/gif', cache_timeout=0), {'Cache-Control': f'no-cache, max-age=0, t={timestamp}'}
 
 if __name__ == '__main__':
     app.run()
